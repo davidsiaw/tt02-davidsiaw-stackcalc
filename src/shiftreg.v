c040f9b152005a6f255d5905b978ff15c3f5c26d
David Siaw <davidsiaw@gmail.com>
2022-11-22 21:22:08 +0900

    describe stack as array of shifts

---
+++ src/shiftreg.v
@@ -0,0 +1,22 @@
+module shiftreg #(parameter SIZE=8) (
+  input d,
+  input clk,
+  input en,
+  input dir,
+  input rst,
+  output reg [SIZE-1:0] q);
+
+   always @ (posedge clk) begin
+      if (rst)
+        q <= 0;
+      else begin
+        if (en)
+          case (dir)
+            1'b0: q <= { q[SIZE-2:0], d };
+            1'b1: q <= { d, q[SIZE-1:1] };
+          endcase
+        else
+          q <= q;
+      end
+    end
+endmodule
