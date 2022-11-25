87f455b918d54c1346d45519b63d37d64bec0795
David Siaw <davidsiaw@gmail.com>
2022-11-25 13:22:00 +0900

    implement swap, peek, dupl

--- src/stack_cpu.v
+++ src/stack_cpu.v
@@ -1,4 +1,22 @@
 
+module word_multiplexer (
+  input [3:0] a, b, c, d,
+  input [1:0] s,
+  output reg [3:0] q
+);
+
+  always @ (*) begin
+    casez(s)
+      2'b00: q = a;
+      2'b01: q = b;
+      2'b10: q = c;
+      2'b11: q = d;
+      default: q = 3'b000;
+    endcase;
+  end
+
+endmodule
+
 
 module stack_cpu (
   input wire [7:0] io_in,
@@ -20,15 +38,29 @@ module stack_cpu (
 
 
   wire [3:0] v0, v1;
-  reg move_flag, dir_flag;
+  reg move_flag, dir_flag, swap_flag;
+
+  wire [3:0] stack_input;
   stack_register stack(
     .clk(clk),
     .rst(rst),
     .mode(dir_flag), // 1 push, 0 pop
     .move(move_flag),
-    .in_word(inbits),
+    .swap(swap_flag),
+    .in_word(stack_input),
     .top_word(v0),
     .second_word(v1)
+  );
+
+  // stack input selection
+  reg [1:0] input_select;
+  word_multiplexer stack_input_select(
+    .a(inbits),
+    .b(v0),
+    .c(v1),
+    .d({ 1'b0, 1'b0, 1'b0, 1'b0 }), // status register
+    .s(input_select),
+    .q(stack_input)
   );
 
   // processor state
@@ -45,9 +77,12 @@ module stack_cpu (
       op_counter <= 0;
       move_flag <= 0;
       dir_flag <= 0;
+      swap_flag <= 0;
+      input_select <= 0;
     end
     else if (fetch_flag == 1) begin
-      // fetch the next op
+      // spend one cycle to fetch the next op
+      // and latch it in
       current_op <= inbits;
       op_counter <= 0;
       fetch_flag <= 0;
@@ -56,10 +91,11 @@ module stack_cpu (
       op_counter <= op_counter + 1;
 
       // decode op
-      if (current_op == 4'h01) begin
+      if (current_op == 4'h1) begin
         // PUSH
 
         if (op_counter == 0) begin
+          input_select <= 0;
           move_flag <= 1;
           dir_flag <= 1;
         end
@@ -69,7 +105,7 @@ module stack_cpu (
         end
         
       end
-      else if (current_op == 4'h02) begin
+      else if (current_op == 4'h2) begin
         // POP
 
         if (op_counter == 0) begin
@@ -82,10 +118,57 @@ module stack_cpu (
         end
 
       end
-      else if (current_op == 4'h03) begin
-        // OUT
-        out_dff <= v0;
+      else if (current_op == 4'h3) begin
+        // OUTL
+        out_dff <= { out_dff[7:4], v0 };
         fetch_flag <= 1; // complete
+
+      end
+      else if (current_op == 4'h4) begin
+        // OUTH
+        out_dff <= { v0, out_dff[3:0] };
+        fetch_flag <= 1; // complete
+
+      end
+      else if (current_op == 4'h5) begin
+        // SWAP
+
+        if (op_counter == 0) begin
+          swap_flag <= 1;
+        end
+        else begin
+          swap_flag <= 0;
+          fetch_flag <= 1; // complete
+        end
+
+      end
+      else if (current_op == 4'h6) begin
+        // PEEK
+
+        if (op_counter == 0) begin
+          input_select <= 2;
+          move_flag <= 1;
+          dir_flag <= 1;
+        end
+        else begin
+          move_flag <= 0;
+          fetch_flag <= 1; // complete
+        end
+
+      end
+      else if (current_op == 4'h7) begin
+        // DUP
+
+        if (op_counter == 0) begin
+          input_select <= 1;
+          move_flag <= 1;
+          dir_flag <= 1;
+        end
+        else begin
+          move_flag <= 0;
+          fetch_flag <= 1; // complete
+        end
+
       end
       else begin
         // all unknown instructions are NOOP
@@ -100,3 +183,22 @@ module stack_cpu (
   assign io_out = out_dff;
 
 endmodule
+
+// ADD ADDC
+// SUB SUBC
+// AND
+// OR
+// XOR
+
+// SHR
+// SHL
+// ROL
+// ROR
+
+// NOT
+// INC
+// DEC
+
+
+// CMP
+// EX extended opcodes
--- src/tb.v
+++ src/tb.v
@@ -11,10 +11,11 @@ module tb (
   input globclk,
   input clk,
   input rst,
+  input [31:0] testnumber,
   input [31:0] select,
   input [3:0] io_ins,
   input mode,
-  output [3:0] io_outs,
+  output [7:0] io_outs,
   output [3:0] io_outs2
    );
 
@@ -29,12 +30,13 @@ module tb (
   wire [7:0] inputs = {1'b0, 1'b0, io_ins, rst, clk};
   wire [7:0] outputs;
 
-  // instantiate the DUT
+  // dut 0x1 is cpu
   stack_cpu dut1(
     .io_in  (inputs),
     .io_out (outputs)
   );
 
+  // dut 0x2 is stack register alone
   wire [3:0]regout1;
   wire [3:0]regout2;
   stack_register dut2(
@@ -46,7 +48,8 @@ module tb (
     .second_word(regout2)
   );
 
-  assign io_outs = {4{select[0]}} & outputs[3:0] |
+
+  assign io_outs = {8{select[0]}} & outputs |
            {4{select[1]}} & regout1;
 
 endmodule
