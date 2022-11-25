e39b76fa174128558293e2d6ebeeb7db346aa289
David Siaw <davidsiaw@gmail.com>
2022-11-25 15:11:45 +0900

    constants file

---
+++ src/constants.v
@@ -0,0 +1,16 @@
+`define STACK_SIZE 8
+
+`define STACK_MODE_IDLE  3'b000
+`define STACK_MODE_PUSH  3'b001
+`define STACK_MODE_POP   3'b010
+`define STACK_MODE_SWAP  3'b011
+`define STACK_MODE_RESET 3'b111
+
+`define SELECT_INPUT_BITS 3'b000
+`define SELECT_STACK_TOP0 3'b001
+`define SELECT_STACK_TOP1 3'b010
+`define SELECT_CALC_STORE 3'b011
+`define SELECT_E 3'b100
+`define SELECT_F 3'b101
+`define SELECT_G 3'b110
+`define SELECT_H 3'b111
