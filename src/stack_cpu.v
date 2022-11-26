e0a46cc5ab6b1a61f273ef4c4b5f5a66105e7819
David Siaw <davidsiaw@gmail.com>
2022-11-26 15:28:35 +0900

    reorganize memory into its own file

--- src/stack_cpu.v
+++ src/stack_cpu.v
@@ -1,41 +1,7 @@
 `ifndef CONSTANTS
   `define CONSTANTS
   `include "constants.v"
-`endif  
-
-`define MEMORY_ADDR_BITS 3
-
-`define MEMORY_MODE_NONE  2'b00
-`define MEMORY_MODE_READ  2'b01
-`define MEMORY_MODE_WRITE 2'b10
-`define MEMORY_MODE_CLEAR 2'b11
-
-module memory (
-  input clk,
-  input [1:0] mode,
-  input [`MEMORY_ADDR_BITS-1:0] address,
-  input [3:0] data_in,
-  output reg [3:0] data_out);
-
-  generate
-    genvar i;
-    for(i = 0; i < 2**`MEMORY_ADDR_BITS; i = i + 1) begin
-      reg [3:0] memory_cell;
-
-      always @ (posedge clk) begin
-        if (address == i) begin
-          case(mode)
-            `MEMORY_MODE_NONE:  data_out <= data_out;
-            `MEMORY_MODE_READ:  data_out <= memory_cell;
-            `MEMORY_MODE_WRITE: memory_cell <= data_in;
-            `MEMORY_MODE_CLEAR: data_out <= 0;
-            default: data_out <= data_out;
-          endcase;
-        end
-      end
-    end
-  endgenerate
-endmodule
+`endif
 
 module stack_cpu (
   input wire [7:0] io_in,
