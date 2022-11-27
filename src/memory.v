f32ccc8c2a34d1f8ce5c387fcf3ac7de4a8bc9ba
David Siaw <davidsiaw@gmail.com>
2022-11-27 18:38:07 +0900

    try and fix constraint

--- src/memory.v
+++ src/memory.v
@@ -3,73 +3,62 @@
   `include "constants.v"
 `endif
 
-module memory_cell(
-  input rst,
-  input write,
-  input [3:0] data_in,
-  output reg [3:0] data_out
-);
-  reg [3:0] memory_cell;
-
-  always @ (*) begin
-    if (rst) begin
-      memory_cell = 0;
-    end
-    else if (write) begin
-      memory_cell = data_in;
-    end
-    else begin
-      data_out = memory_cell;
-    end
-  end
-endmodule
-
 module memory (
   input clk,
   input [1:0] mode,
   input [`MEMORY_ADDR_BITS-1:0] address,
   input [3:0] data_in,
-  output reg [3:0] data_out);
-
-  generate
-  	genvar i;
-  	for(i = 0; i < 2**`MEMORY_ADDR_BITS; i = i + 1) begin
-
-      reg [3:0] cell_in;
-      wire [3:0] cell_out;
-      reg rst;
-      reg write;
-
-      memory_cell celll(
-        .rst(rst),
-        .write(write),
-        .data_in(cell_in),
-        .data_out(cell_out)
-      );
-
-      always @ (*) begin
-        rst = mode == `MEMORY_MODE_CLEAR;
-        if (address == i) begin
-          write = mode == `MEMORY_MODE_WRITE;
-          cell_in = data_in;
-          if (!write) begin
-            data_out = cell_out;
-          end
-        end
-        else begin
-          write = 0;
-          cell_in = cell_out;
-        end
-      end
-
-  	end
-  endgenerate
+  output reg [3:0] data_out
+);
+  
+  reg [3:0] memory_cell0;
+  reg [3:0] memory_cell1;
+  reg [3:0] memory_cell2;
+  reg [3:0] memory_cell3;
+  reg [3:0] memory_cell4;
+  reg [3:0] memory_cell5;
+  reg [3:0] memory_cell6;
+  reg [3:0] memory_cell7;
 
   always @ (posedge clk) begin
-	  case(mode)
-  		`MEMORY_MODE_NONE:  data_out <= data_out;
-  		`MEMORY_MODE_CLEAR: data_out <= 0;
-  		default: data_out <= data_out;
-	  endcase;
+    if (mode == `MEMORY_MODE_CLEAR) begin
+      memory_cell0 <= 0;
+      memory_cell1 <= 0;
+      memory_cell2 <= 0;
+      memory_cell3 <= 0;
+      memory_cell4 <= 0;
+      memory_cell5 <= 0;
+      memory_cell6 <= 0;
+      memory_cell7 <= 0;
+      data_out <= 0;
+    end
+    else if (mode == `MEMORY_MODE_READ) begin
+      case(address)
+        3'b000: data_out <= memory_cell0;
+        3'b001: data_out <= memory_cell1;
+        3'b010: data_out <= memory_cell2;
+        3'b011: data_out <= memory_cell3;
+        3'b100: data_out <= memory_cell4;
+        3'b101: data_out <= memory_cell5;
+        3'b110: data_out <= memory_cell6;
+        3'b111: data_out <= memory_cell7;
+      endcase;
+    end
+    else if (mode == `MEMORY_MODE_WRITE) begin
+      case(address)
+        3'b000: memory_cell0 <= data_in;
+        3'b001: memory_cell1 <= data_in;
+        3'b010: memory_cell2 <= data_in;
+        3'b011: memory_cell3 <= data_in;
+        3'b100: memory_cell4 <= data_in;
+        3'b101: memory_cell5 <= data_in;
+        3'b110: memory_cell6 <= data_in;
+        3'b111: memory_cell7 <= data_in;
+      endcase;
+    end
+    else begin
+      data_out <= data_out;
+    end
   end
+
 endmodule
